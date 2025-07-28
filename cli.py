e5e5078118adb61dd30f60768b6e02896e5437ad
Ben <bmwangi676@gmail.com>
2025-07-28 23:48:02 +0300

    Added Command Line Arguments -a, -r

--- cli.py
+++ cli.py
@@ -5,7 +5,21 @@ import sys
 import argparse
 
 
-task = sys.argv[1]
-file = open("tasks.json", "a")
-file.write(f'"{task}"\n')
-file.close
+if len(sys.argv) == 1:
+    print(
+        "Usage: type cli.py -h/--help")
+    
+else:
+    parser = argparse.ArgumentParser(description ="Task Management system")
+    parser.add_argument("-a", help = "Add task")
+    parser.add_argument("-r", help = "Lists all pending tasks")
+    args = parser.parse_args()
+
+    if args.a:
+        task = args.a
+        with open("tasks.txt", "a") as file:
+                file.write(f'{task}\n')
+    elif args.r:
+        with open("tasks.txt", "r") as file:
+                for line in file:
+                    print(f"Current remaining tasks: {line}", end ="")
